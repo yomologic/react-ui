a0a681e5b758bdc0fba310810952892cb06c67ad
yadmin-yomologic <admin@yomologic.com>
2025-11-14 17:41:32 -0600

    Fix vercel-setup.js to find root package.json using __dirname

--- vercel-setup.js
+++ vercel-setup.js
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const path = require('path');
+
+// Find the root package.json (go up one level from site/)
+const rootPackagePath = path.join(__dirname, 'package.json');
 
 // Read package.json
-const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+const pkg = JSON.parse(fs.readFileSync(rootPackagePath, 'utf8'));
 
 // Set private to true for workspace support
 pkg.private = true;
 
 // Write back
-fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2) + '\n');
+fs.writeFileSync(rootPackagePath, JSON.stringify(pkg, null, 2) + '\n');
 
 console.log('✓ Set package.json to private for workspace build');
